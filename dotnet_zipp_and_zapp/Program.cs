7585324b47838366cc5cca1d355d4318e71e8392
KiwiGeek <jpenman@lcg.org>
2021-08-25 23:04:55 -0400

    Resolved Warnings and Messages.

--- dotnet_zipp_and_zapp/Program.cs
+++ dotnet_zipp_and_zapp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace dotnet_zipp_and_zapp
@@ -14,14 +13,16 @@ namespace dotnet_zipp_and_zapp
 		[Flags]
 		private enum MfbWindowFlags
 		{
+			// ReSharper disable UnusedMember.Local
 			WF_RESIZABLE = 0x01,
 			WF_FULLSCREEN = 0x02,
 			WF_FULLSCREEN_DESKTOP = 0x04,
 			WF_BORDERLESS = 0x08,
 			WD_ALWAYS_ON_TOP = 0x16
+			// ReSharper restore UnusedMember.Local
 		}
 
-		[DllImport(@"minifb.dll", CharSet = CharSet.Ansi)]
+		[DllImport(@"minifb.dll", BestFitMapping = false, ThrowOnUnmappableChar = true)]
 		private static extern IntPtr mfb_open_ex(string title, uint width, uint height, MfbWindowFlags flags);
 
 		[DllImport(@"minifb.dll")]
@@ -31,12 +32,12 @@ namespace dotnet_zipp_and_zapp
 		private static extern bool mfb_wait_sync(IntPtr window);
 
 		static readonly IntPtr BackBuffer = Marshal.AllocHGlobal(GAME_RES_WIDTH * GAME_RES_HEIGHT * GAME_RES_BPP);
-		private static readonly Random rnd = new Random();
+		private static readonly Random Rnd = new();
 		
 		static void Main()
 		{
 			//Console.WriteLine("Hello World!");
-			IntPtr windowHandle = mfb_open_ex("The Adventures of Zipp and Zapp", 1920, 1080, MfbWindowFlags.WF_FULLSCREEN);
+			IntPtr windowHandle = mfb_open_ex("The Adventures of Zipp and Zapp", 800, 600, MfbWindowFlags.WF_RESIZABLE);
 			if (windowHandle == (IntPtr)0)
 			{
 				return;
@@ -44,17 +45,16 @@ namespace dotnet_zipp_and_zapp
 
 			do
 			{
-				
-				/*for (int y = 0; y < 240; y++)
+				for (int y = 0; y < 240; y++)
 				{
 					DrawHorizontalLine(0, 384, y, (byte)y, (byte)(240 - y), 0);
-				}*/
+				}
 
 				for (int y = 0; y < GAME_RES_HEIGHT; y++)
 				{
 					for (int x = 0; x < GAME_RES_WIDTH; x++)
 					{
-						DrawPixel(x, y, (byte)rnd.Next(0,255), (byte)rnd.Next(0,255), (byte)rnd.Next(0,255));
+						DrawPixel(x, y, (byte)Rnd.Next(0,255), (byte)Rnd.Next(0,255), (byte)Rnd.Next(0,255));
 					}
 				}
 
@@ -74,9 +74,9 @@ namespace dotnet_zipp_and_zapp
 			int startingPixel = (y * GAME_RES_WIDTH + startX) * GAME_RES_BPP;
 			for (int x = startX; x < endX; x++)
 			{
-				Marshal.WriteByte(BackBuffer+ startingPixel + x * GAME_RES_BPP, blue);		// blue
-				Marshal.WriteByte(BackBuffer+ startingPixel + x * GAME_RES_BPP + 1, green);	// green
-				Marshal.WriteByte(BackBuffer+ startingPixel + x * GAME_RES_BPP + 2, red);	// red
+				Marshal.WriteByte(BackBuffer + startingPixel + x * GAME_RES_BPP, blue);     // blue
+				Marshal.WriteByte(BackBuffer + startingPixel + x * GAME_RES_BPP + 1, green);    // green
+				Marshal.WriteByte(BackBuffer + startingPixel + x * GAME_RES_BPP + 2, red);  // red
 			}
 		}
 
